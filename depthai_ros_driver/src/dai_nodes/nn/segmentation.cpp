6a2d9f7503836e2b491c1a804dd07ba46c1c4911
Joseph Duchesne <joseph@upsiderobotics.com>
2025-09-05 19:42:47 +0000

    Updated segmentation hardcoded patch

--- depthai_ros_driver/src/dai_nodes/nn/segmentation.cpp
+++ depthai_ros_driver/src/dai_nodes/nn/segmentation.cpp
@@ -44,6 +44,17 @@ Segmentation::Segmentation(const std::string& daiNodeName,
     description->model = ph->getParam<std::string>("i_nn_model");
     segNode = pipeline->create<dai::node::NeuralNetwork>()->build(camNode.getUnderlyingNode(), *description);
     imageManip = pipeline->create<dai::node::ImageManip>();
+    float crop_sides = (1920/2 - 512/2)/2;  // crop ~23% off the sides
+    float crop_top = (1080/2 - 384);   // crop ~30% off the top (sky)
+
+    imageManip->initialConfig->addCrop(crop_sides, crop_top, 512, 384);
+
+    // imageManip->initialConfig.setResize(512, 384, dai::ImageManipConfig::ResizeMode::STRETCH);           // resize to 512x384
+
+    // imageManip->initialConfig.setResize(512, 384);
+    // imageManip->initialConfig.setCropRect(0.0f, 0.0f, 1.0f, 1.0f);
+    // imageManip->initialConfig.setResizeThumbnail(512, 384, 0, 0, 0);
+    // imageManip->initialConfig.setKeepAspectRatio(false);
     RCLCPP_DEBUG(getLogger(), "Node %s created", daiNodeName.c_str());
     imageManip->out.link(segNode->input);
     setInOut(pipeline);
@@ -94,7 +105,9 @@ void Segmentation::segmentationCB(const std::string& name, const std::shared_ptr
     nnFrame.reshape({width, height});
     cv::Mat nn_mat = cv::Mat(nnFrame.shape()[0], nnFrame.shape()[1], CV_32SC1, nnFrame.data());
     auto classNum = seg->getTensor<int32_t>(layers[1], true).shape()[1];
+    nn_mat = nn_mat.reshape(0, 384);
     cv::Mat cv_frame = decodeDeeplab(nn_mat, classNum);
+
     auto currTime = getROSNode()->get_clock()->now();
     cv_bridge::CvImage imgBridge;
     sensor_msgs::msg::Image img_msg;
